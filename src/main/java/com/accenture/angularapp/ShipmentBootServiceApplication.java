1bf53fada0eae1403ae89b32a90e67b4c71d8f23
pradeep041981 <pradeep.a.ramaiah@accenture.com>
2026-02-20 15:28:18 +0530

    Rename application and update artifact details to reflect shipmentBootService

--- src/main/java/com/accenture/angularapp/ShipmentBootServiceApplication.java
+++ src/main/java/com/accenture/angularapp/ShipmentBootServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AngularAppApplication {
+public class ShipmentBootServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AngularAppApplication.class, args);
+        SpringApplication.run(ShipmentBootServiceApplication.class, args);
     }
 
 }
